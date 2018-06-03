a5a5f07f35d4ad3b415fe5d53c53733a067621af
Yannick Said <ysaid@u-pro.fr>
2018-06-03 02:34:42 +0200

    Save view

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,33 @@
 import 'bootstrap-toggle/js/bootstrap-toggle.js';
 
-let MainController = () => {
-    let toogleInput = () => {
+let MainController = function () {
+    let toogleInput = function () {
         $('input[data-toggle="toggle"]').change(function () {
             console.log("Change");
         });
         return false;
     }
+    let collapse = function () {
+        $('.collapse').on('shown.bs.collapse', function () {
+            $(this).parent().find('#btn-' + $(this).attr('id')).html('<i class="fa fa-minus-circle"></i>');
+        }).on('hidden.bs.collapse', function () {
+            $(this).parent().find('#btn-' + $(this).attr('id')).html('<i class="fa fa-plus-circle"></i>');
+        });
+    }
+
+    
+    let oTable = function () {
+      
+    }
 
     return {
-        init: () => {
+        init: function () {
             toogleInput();
+            collapse();
         }
     }
-}
-MainController.init();
+}();
+
+$(function () {
+    MainController.init();
+});
